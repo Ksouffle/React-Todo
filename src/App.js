f1e804811773e96dde540eef8589c240bbaa5146
Ethan Karp <ethansmainemail@gmail.com>
2021-02-21 21:48:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoList";
+
+// let todos = [];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [1, 2, 3],
+    };
+  }
+  mapTodos() {
+    this.state.todos.forEach((item) => <h1 key={item}>todo</h1>);
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="app">
         <h2>Welcome to your Todo App!</h2>
+        {console.log(this.state.todos)}
+        <TodoList todos={this.state.todos} />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
+import React, { Component } from "react";
+import Todo from "./Todo";
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+class TodoList extends Component {
+  mapTodos() {
+    this.props.todos.map((item) => <Todo />);
+  }
+
+  render() {
+    return <h1>{this.mapTodos()}</h1>;
+  }
+}
+
+export default TodoList;
